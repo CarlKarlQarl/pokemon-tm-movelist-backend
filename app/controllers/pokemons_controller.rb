269abd3723ac48a0ec18e5f40166334325aa0f43
CarlKarlQarl <karlmatthes@gmail.com>
2020-02-01 18:41:18 -0700

    Create, destroy, and strong params added

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,4 +3,21 @@ class PokemonsController < ApplicationController
         all_pokemon = Pokemon.all
         render json: { pokemons: all_pokemon }
     end
+
+    def create
+        @pokemon = Pokemon.create pokemon_params
+        render json: { pokemon: @pokemon }
+    end
+
+    def destroy
+        @pokemon = Pokemon.find(params[:id])
+        @pokemon.destroy
+        render json: { message: "Pokemon removed" }
+    end
+
+    private
+
+    def pokemon_params
+        params.require(:pokemon).permit([:name, :element, :level])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :movelists
   resources :technical_machines
   resources :pokemons
+  root "pokemons#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
